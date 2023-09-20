f85c83e80bda75e1bfcad44eca6b99948ce05550
leirisson <103783610+leirisson@users.noreply.github.com>
2023-09-19 22:57:48 -0400

    primeiros projetos em react

---
+++ organo/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Banner from './components/Banner/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <header className="App-header">
+        <p>
+          Editei a frase !
+        </p>
+        
+        <h1>Leirisson Souza</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './banner.css'
+function Banner(){
+    return (
+        <header className="banner">
+                <img src="/imagens/banner.png"  alt="Banner princiapl da agina do organo"/>
+        </header>
+
+    )
+}
+
+export default Banner
